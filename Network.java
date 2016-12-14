6f9625bd65e250c438ec2c827fe44261c6b40730
Halim <halim.bouguedra@gmail.com>
2016-12-14 23:09:45 +0100

    Add files via upload

--- Network.java
+++ Network.java
@@ -2,11 +2,11 @@ package network;
 
 import java.util.Random;
 
+
 public class Network {
 
 	public int numberOfLayers;
 	public int inputLayerSize;
-	public double[] ideal;
 	public double[] actual;
 	public double[][][] weights;
 	public double[][] biases;
@@ -47,8 +47,8 @@ public class Network {
 		System.out.println("Total number of layers = " + numberOfLayers);
 	}
 	
-	/* METHOD
-	 * takes in an input vector, of the same dimension as 
+	
+	/* takes in an input vector, of the same dimension as 
 	 * the first layer of the network (the input layer), 
 	 * then feeds that input to the next layer, through 
 	 * the weights, adds the biases then applies the Sigmoid 
@@ -78,19 +78,26 @@ public class Network {
 	/*calculates the errors of the network, with  a recursive 
 	 * call, from the given layer to the output layer */ 
 	// <<TO REVISE>>
-	private double[] calculateDeltas(int layer){
-		
+	private double[] calculateDeltas(int layer, double[] input, double[] idealOutput){
 		double[] result = new double[getBiases(layer).length];
 		if(layer == numberOfLayers){
+<<<<<<< HEAD
+			double[] actualOutput = feedForward(input);
+			result = F.hadamard(F.substract(actualOutput, idealOutput),
+=======
 			result = F.hadamard(F.substract(actual, ideal),
+>>>>>>> 99bc89934ee1a15cfc3fca6b1e88806aa4661509
 					    F.sigmoidPrime(getZs(layer)));
 			
 			setDeltas(layer, result);
-			
 		}
 		else{
 			result = F.hadamard(F.mDotv( F.transpose(getWeights(layer + 1)), 
+<<<<<<< HEAD
+						     calculateDeltas(layer + 1, input, idealOutput) ),
+=======
 						     calculateDeltas(layer + 1) ),
+>>>>>>> 99bc89934ee1a15cfc3fca6b1e88806aa4661509
 					    F.sigmoidPrime(getZs(layer)) );
 			setDeltas(layer, result);
 		}
@@ -98,13 +105,77 @@ public class Network {
 	}
 	
 	/* makes a call to the private method above to calculate
-	 * all the errors in the network starting from the first
-	 * hidden layer (layer 2). ignores the return value.  */
+	 * all the errors in the network based on the provided actual 
+	 * and ideal outputs, starting from the first hidden layer (l:2).
+	 * Ignores the return value.  */
 	// <<TO REVISE>>
-	public void  calculateDeltas(){
-		calculateDeltas(2);
+	public void  calculateDeltas(double[] input, double[] idealOutput){
+		calculateDeltas(2, input, idealOutput);
 	}
-
+	
+	// <<TO COMPLETE !>>
+	/* Stochastic Gradient Descent (SGD) implementation. given a learning rate
+	 * and a set of training data (input/ideal output pairs), this method calculates
+	 * the cost function derivative (gradient) for each input, then uses the prior 
+	 * to update the network's weights and biases with the goal of minimizing the cost.*/
+	public void updateNetwork(double learningRate, TrainingData[] training){
+		int batchSize = training.length;
+		actual = new double[batchSize];
+		/* initializes the matrices that will contain the mean 
+		 * value of each cost function derivative with respect
+		 * to individual weights and biases (i.e:  components 
+		 * of the cost function's gradient)*/
+		double[][][] dCdw = zeroWeights();
+		double[][] dCdb = zeroBiases();
+		double input[];
+		double ideal[];
+		for(int i = 0; i < batchSize; i++){
+			input = training[i].getInput();
+			actual = feedForward(input);
+			ideal = training[i].getDesired();
+			calculateDeltas(input, ideal);
+			for(int j = 0; j < numberOfLayers - 1; j++){
+				for(int k = 0; k < biases[j].length; k++){
+					dCdb[j][k] += deltas[j][k];
+					for(int n = 0; n < weights[j][k].length; n++){
+				    	dCdw[j][k][n] += activations[j][n] * deltas[j][k];
+					}
+				}
+			}
+		}
+		
+		for(int i = 0; i < numberOfLayers - 1; i++){
+			for(int j = 0; j < biases[i].length; j++){
+				biases[i][j] -= ((learningRate/batchSize) * dCdb[i][j]);
+				for(int k = 0; k < weights[i][j].length; k++){
+					weights[i][j][k] -= ((learningRate/batchSize) * dCdw[i][j][k]);
+				}
+			}
+		}
+		
+	}
+	
+	private double[][][] zeroWeights(){
+		double[][][] zeros = weights;
+		for(int i = 0; i < weights.length; i++){
+			for(int j = 0; j < weights[i].length; j++){
+				for(int k = 0; k < weights[i][j].length; k++){
+					zeros[i][j][k] = 0.0;
+				}
+			}
+		}
+		return zeros;
+	}
+	
+	private double[][] zeroBiases(){
+		double[][] zeros = biases;
+		for(int j = 0; j < biases.length; j++){
+			for(int k = 0; k < biases[j].length; k++){
+				zeros[j][k] = 0.0;
+			}
+		}
+		return zeros;
+	}
 	public double[] getBiases(int layer){
 		return biases[layer - 2];
 	}
@@ -135,7 +206,6 @@ public class Network {
 	public void setActivations(int layer, double[] values){
 		activations[layer - 1] =  values;
 	}
-	
 
 	@Override
 	public String toString(){
