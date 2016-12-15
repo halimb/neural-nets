f8ddd3be22e3dd4c6d948a60669096e256a0b157
Halim <halim.bouguedra@gmail.com>
2016-12-15 01:34:00 +0100

    Add files via upload

---
+++ TrainingData.java
@@ -0,0 +1,19 @@
+package network;
+
+public class TrainingData {
+	private double[] input;
+	private double[] desired;
+	
+	public TrainingData(double[] input, double[] desired){
+		this.input = input;
+		this.desired = desired;
+	}
+	
+	public double[] getInput() {
+		return input;
+	}
+	public double[] getDesired() {
+		return desired;
+	}
+
+}
